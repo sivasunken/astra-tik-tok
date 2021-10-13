7eaba77f3d0b605042d1bbf7b8c307fbef1d58e9
Sivasankar Sabapathy <sivasankar.rs@gmail.com>
2021-10-13 16:48:44 +0000

    Created pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
+import Header from './components/Header'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <HashRouter>
+      <Header />
+      <Switch>
+        <Route path="/upload" component={Upload} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </HashRouter>
   );
 }
 
-export default App;
+export default App
